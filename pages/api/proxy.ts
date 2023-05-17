4f0f9fa43d4c6c66d82216b7ef5830c977d60967
gexiaopeng <gexiaopeng@tom.com>
2023-05-17 15:09:29 +0800

    ok

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,15 +1,14 @@
-import http from 'http'
-import https from 'https'
-import { NextRequest,NextResponse } from "next/server";
+import * as http from 'http'
 
-export default function (req:NextRequest, res:NextResponse) {
+
+export default function (req, res) {
   const options =  {
     hostname: 'localhost',
     method: req.method,
     headers: req.headers,
   };
 
-  const client = https.request;
+  const client = http.request;
 
   const proxyReq = client(req.url, function (proxyRes) {
     res.writeHead(proxyRes.statusCode, proxyRes.headers)
