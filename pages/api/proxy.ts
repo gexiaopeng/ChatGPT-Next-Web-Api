8022f387b1109c1d2f0d23049cc9949e921b72b8
gexiaopeng <gexiaopeng@tom.com>
2023-05-17 14:21:11 +0800

    ok

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,26 +1,18 @@
 import http from 'http'
 import https from 'https'
+import { NextRequest,NextResponse } from "next/server";
 
+export default function (req:NextRequest, res:NextResponse) {
+  const options = req.options;
 
-export default function (req, res) {
-  const options = {
-    headers: req.headers,
-    method: req.method,
-    // 将代理服务器作为目标代理
-    host: req.hostname,
-    port: req.port,
-    path: req.url,
-    protocol:req.protocol
-  }
+  const client = options.protocol === 'https:' ? https.request : http.request;
 
-  const client = options.protocol === 'https:' ? https.request : http.request
-
-  const proxyReq = client(options, function (proxyRes) {
+  const proxyReq = client(options, function (proxyRes:NextResponse) {
     res.writeHead(proxyRes.statusCode, proxyRes.headers)
     proxyRes.pipe(res, {
       end: true
-    })
+    });
   })
 
-  req.pipe(proxyReq, { end: true })
+  req.pipe(proxyReq, { end: true });
 }
