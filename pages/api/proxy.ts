ef5f24743d5a314851bacc0afc6c4077b8d12ef4
gexiaopeng <gexiaopeng@tom.com>
2023-05-18 15:40:24 +0800

    ok

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,40 +1,9 @@
-import  http from 'http'
-import  https from 'https'
-import url from 'url'
-import next from 'next'
-const dev = process.env.NODE_ENV !== 'production'
-
-// Init the Next app:
-const app = next({ dev })
-
-function request (req:any, res:any) {
-  const u = url.parse(req.url);
-  console.log("---proxy----url:",req.url);
-  const protocol=u.protocol;
-
-  const options = {
-    hostname : u.hostname,
-    port     : u.port || 80,
-    path     : u.path,
-    method     : req.method,
-    headers     : req.headers
-  };
-
-  const client = protocol=== 'https:' ? https.request : http.request;
-
-  const proxyReq = client(options, function (proxyRes:any) {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res, {
-      end: true
-    });
-  })
-
-  req.pipe(proxyReq, { end: true });
+import fetch from 'node-fetch';
+import { NextRequest, NextResponse } from "next/server";
+export default async function handler(req, res) {
+  const { method } = req;
+  const { PROXY_API_URL } = process.env;
+  console.log("-method-"+method+",PROXY_API_URL:"+PROXY_API_URL+",url:"+req.url);
+  const proxyResponseJson = {code:1,msg:'ok'};
+  res.status(200).json(NextResponse.json({code:1,msg:"hello"}).json());
 }
-console.log("--createServer--begin--",http,https);
-http.createServer().on('request', request).listen(3888);
-console.log("--createServer--end--",http,https);
-
-app.prepare().then(() => {
-  console.log("app--prepare");
-});
