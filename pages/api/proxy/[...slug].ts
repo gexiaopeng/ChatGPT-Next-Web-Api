3ef4f6c4440eb99d0a4600314b5a9a6a4b3fd1ef
gexiaopeng <gexiaopeng@tom.com>
2023-05-18 16:30:41 +0800

    ok

--- pages/api/proxy/[...slug].ts
+++ pages/api/proxy/[...slug].ts
@@ -1,18 +1,29 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware, ErrorRequestHandler, RequestHandler } from 'http-proxy-middleware';
 import { NextApiRequest, NextApiResponse } from 'next';
+type AsyncRequestHandler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
   const { method } = req;
   const { PROXY_API_URL } = process.env;
   const { slug } = req.query;
   console.log("-method-"+method+",PROXY_API_URL:"+PROXY_API_URL+",url:"+req.url+",slug:"+slug!);
-  // 创建中间件代理到外部 API
-  const proxy = createProxyMiddleware({
+
+  const proxyMiddleware = createProxyMiddleware({
     target: `${slug!}`,
     changeOrigin: true,
     headers: { 'X-Custom-Header': 'Hello from Vercel' },
   });
-
-  // 执行中间件
-  proxy(req, res);
+  const proxy: AsyncRequestHandler = (req, res) => {
+    return new Promise<void>((resolve, reject) => {
+      const callback: ErrorRequestHandler = (err:any, req:any, res:any) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      };
+      proxyMiddleware(req, res, callback);
+    });
+  };
+  return proxy(req, res);
 }
