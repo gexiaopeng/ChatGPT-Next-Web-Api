3afa468669150e1a53029c6c119702c5bc8705aa
gexiaopeng <gexiaopeng@tom.com>
2023-05-18 17:13:50 +0800

    ok

--- pages/api/proxy/[...slug].ts
+++ pages/api/proxy/[...slug].ts
@@ -6,15 +6,16 @@ export default async function handler(req:any, res:any) {
   const { method } = req;
   const { query } = req
   const { slug } = query
-  console.log("-method-"+method+",url:"+req.url+",slug:"+slug+",query:"+query,req.headers);
-  /**
-  const proxyResponse = await fetch(`${slug}`, {
+  console.log("-method-"+method+",url:"+req.url+",slug:["+slug+"]");
+  console.log("-query-",query);
+  const url=req.url.splice(11);
+
+  const proxyResponse = await fetch(url, {
     method,
     headers: req.headers,
     body: method.toLowerCase() === 'get' ? null : req.body,
   });
+  console.log("-headers-",await proxyResponse.headers);
   const proxyResponseJson = await proxyResponse.json();
-   */
-  const proxyResponseJson={code:90,msg:'hello'};
-  res.status(200).json(proxyResponseJson);
+ res.status(proxyResponseJson.status).json(proxyResponseJson);
 }
