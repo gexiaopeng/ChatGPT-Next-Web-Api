a20d36276fdeb37535fa70338b149c2cac24c927
gexiaopeng <gexiaopeng@tom.com>
2023-05-18 16:42:00 +0800

    ok

--- pages/api/proxy/[...slug].ts
+++ pages/api/proxy/[...slug].ts
@@ -1,25 +1,16 @@
-import { createProxyMiddleware, RequestHandler } from 'http-proxy-middleware';
-import { NextApiRequest, NextApiResponse } from 'next';
-type AsyncRequestHandler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+
+import fetch from 'node-fetch';
+
+export default async function handler(req:any, res:any) {
 
   const { method } = req;
-  const { PROXY_API_URL } = process.env;
   const { slug } = req.query;
-  console.log("-method-"+method+",PROXY_API_URL:"+PROXY_API_URL+",url:"+req.url+",slug:"+slug!);
-
-  const proxyMiddleware = createProxyMiddleware({
-    target: `${slug!}`,
-    changeOrigin: true,
-    headers: { 'X-Custom-Header': 'Hello from Vercel' },
+  console.log("-method-"+method+",url:"+req.url+",slug:"+slug!);
+  const proxyResponse = await fetch(`${slug}`, {
+    method,
+    headers: req.headers,
+    body: method.toLowerCase() === 'get' ? null : req.body,
   });
-  const proxy: AsyncRequestHandler = (req, res) => {
-    return new Promise<void>((resolve, reject) => {
-      const callback: RequestHandler = (req:any, res:any) => {
-          resolve();
-      };
-      proxyMiddleware(req, res, callback);
-    });
-  };
-  return proxy(req, res);
+  const proxyResponseJson = await proxyResponse.json();
+  res.status(proxyResponse.status).json(proxyResponseJson);
 }
