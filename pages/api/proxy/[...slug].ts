9815fef9d4a7759f262040c6c9b76d4d89919933
gexiaopeng <gexiaopeng@tom.com>
2023-05-18 17:38:18 +0800

    ok

--- pages/api/proxy/[...slug].ts
+++ pages/api/proxy/[...slug].ts
@@ -17,6 +17,9 @@ export default async function handler(req:any, res:any) {
     body: method.toLowerCase() === 'get' ? null : req.body,
   });
   //console.log("-proxyResponse-",await proxyResponse);
-  const proxyResponseJson = await proxyResponse.json();
- res.status(proxyResponseJson.status).json(proxyResponseJson);
+  const proxyResponseHeaders = await proxyResponse.headers;  // 获取响应头
+  const proxyResponseBody = await proxyResponse.text();  // 获取响应体
+
+  // 将响应头和响应体一起返回
+  res.status(proxyResponse.status).set(proxyResponseHeaders).send(proxyResponseBody);
 }
