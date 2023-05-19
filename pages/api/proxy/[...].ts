e6736058753a46aa57e36686a6671ea5b62dbc8a
gexiaopeng <gexiaopeng@tom.com>
2023-05-19 13:11:42 +0800

    ok

--- pages/api/proxy/[...].ts
+++ pages/api/proxy/[...].ts
@@ -1,19 +1,32 @@
+import {NextRequest, NextResponse} from 'next/server';
 
-import { NextRequest } from 'next/server';
 export default async function handler(req: NextRequest) {
-  const { method } = req;
-  console.log("-method-"+method+",url:["+req.url+"]");
-  //console.log("-query-",query);
-  //console.log("-res-",res);
-  const url = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll("/api/proxy/", "");
-  console.log("-url-["+url+"]");
-  return fetch(url, {
-    headers: req.headers,
-    method:method,
-    body: req.body,
-  });
+    const {method} = req;
+    console.log("-method-" + method + ",url:[" + req.url + "]");
+    //console.log("-query-",query);
+    //console.log("-res-",res);
+    const url = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll("/api/proxy/", "");
+    console.log("-url-[" + url + "]");
+    try {
+        return fetch(url, {
+            headers: req.headers,
+            method: method,
+            body: req.body,
+        });
+    } catch (e) {
+        console.error("[api/proxy]", e);
+        return NextResponse.json(
+            {
+                error: true,
+                msg: JSON.stringify(e),
+            },
+            {
+                status: 500,
+            },
+        );
+    }
 
 }
 export const config = {
-  runtime: 'edge',
+    runtime: 'edge',
 };
