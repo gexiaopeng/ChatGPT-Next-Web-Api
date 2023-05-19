05daeea05e214329b77d6fbb1b13c3621b6bc6be
gexiaopeng <gexiaopeng@tom.com>
2023-05-19 10:44:51 +0800

    ok

--- pages/api/proxy/[...slug].ts
+++ pages/api/proxy/[...slug].ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
-
-export default async function handler(req:any, res:any) {
+import { NextRequest } from 'next/server';
+export default async function handler(req: NextRequest) {
 
   const { method } = req;
   const { query } = req
@@ -10,21 +10,13 @@ export default async function handler(req:any, res:any) {
   //console.log("-res-",res);
   const url=req.url.slice(11);
   console.log("-url-["+url+"]");
-  const proxyResponse:any = await fetch(url, {
+  return await fetch(url, {
     method,
     headers: req.headers,
     body: method.toLowerCase() === 'get' ? null : req.body,
   });
 
-
-  //console.log("-proxyResponse-",await proxyResponse);
-  const proxyResponseHeaders = await proxyResponse.headers;  // 获取响应头
-  const proxyResponseBody = await proxyResponse.text();  // 获取响应体
-  res.status(proxyResponse.status);
-  /**
-  proxyResponseHeaders.forEach((value:any,name:any) => {
-    res.setHeader(name, value);    // 设置响应头
-  });
-   */
-  res.text(proxyResponseBody);
 }
+export const config = {
+  runtime: 'edge',
+};
